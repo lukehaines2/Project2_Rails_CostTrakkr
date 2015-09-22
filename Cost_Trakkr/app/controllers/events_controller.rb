2f8801515f4fff7261b14a9729ac396004bb5e5b
LukeHaines <lukehaines2@gmail.com>
2015-09-22 15:25:43 +0100

    added rick r

--- Cost_Trakkr/app/controllers/events_controller.rb
+++ Cost_Trakkr/app/controllers/events_controller.rb
@@ -16,9 +16,33 @@ class EventsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+    redirect_to event
+    else
+    redner :edit
+    end
+  end
+
+
+  def destroy
+    event = Event.find(params[:id]).destroy
+    redirect_to event
+  end
+
+  private
+
+  def event_params
+      params.require(:event).permit(:name, :date, :location, :cost)
+    end
 
 end
