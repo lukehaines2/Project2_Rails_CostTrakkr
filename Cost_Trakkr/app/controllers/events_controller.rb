2e326320b8b016c5dbc2399db9e74068c7f504ef
LukeHaines <lukehaines2@gmail.com>
2015-09-22 11:34:16 +0100

    topbar sorted

--- Cost_Trakkr/app/controllers/events_controller.rb
+++ Cost_Trakkr/app/controllers/events_controller.rb
@@ -1,2 +1,24 @@
 class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+  
+  def new
+    @events = Event.new
+  end
+
+  def create
+    event = Event.new(params.require(:event).permit(:name, :date, :location, :cost))
+
+    if event.save
+      redirect_to event
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
 end
