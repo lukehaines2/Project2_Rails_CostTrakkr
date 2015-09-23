56de8fcbc4b8846edcb29d5a1cd96d86b2cbc24d
LukeHaines <lukehaines2@gmail.com>
2015-09-23 02:18:36 +0100

    pushing

--- Cost_Trakkr/app/controllers/users_controller.rb
+++ Cost_Trakkr/app/controllers/users_controller.rb
@@ -25,6 +25,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+    redirect_to user
+    else
+    redner :edit
+    end
+  end
+
   def destroy
     user = User.find(params[:id]).destroy
     redirect_to user
